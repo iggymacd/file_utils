13a05e862a1d277bf62786d473df03a8808174af
Allan MacDonald <allan@allan-VirtualBox.(none)>
2013-01-27 14:22:35 -0400

    first commit

---
+++ lib/directory.dart
@@ -0,0 +1,13 @@
+library file_utils;
+
+
+_isProjectFolder(currentPath) {
+  return (currentPath.endsWith("out") 
+//      || path.endsWith("Header")
+//      || path.endsWith("Images")
+//      || path.endsWith("img")
+//      || path.endsWith("styles")
+  );
+
+}
+
